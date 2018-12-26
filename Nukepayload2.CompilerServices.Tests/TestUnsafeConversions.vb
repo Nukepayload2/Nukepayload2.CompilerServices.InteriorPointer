d86f0c95613f620197336ed31152fc6c683e6d54
Nukepayload2 <1939357182@qq.com>
2018-12-26 22:16:44 +0800

    Added some useful static_cast s

--- Nukepayload2.CompilerServices.Tests/TestUnsafeConversions.vb
+++ Nukepayload2.CompilerServices.Tests/TestUnsafeConversions.vb
@@ -21,6 +21,141 @@ Public Class TestUnsafeConversions
         pubObj._hiddenReadOnlyValue = 678
         Assert.AreEqual(678, priObj.HiddenReadOnlyValue)
     End Sub
+
+    <TestMethod>
+    Sub TestStaticCastByte()
+        Dim b As Byte = 255
+        Dim sb As SByte
+        b.UnsafeStaticCast(sb)
+        Assert.AreEqual(CSByte(-1), sb)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastSByte()
+        Dim sb As SByte = -1
+        Dim b As Byte
+        sb.UnsafeStaticCast(b)
+        Assert.AreEqual(CByte(255), b)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastShort()
+        Dim us As UShort = &HFFFF
+        Dim s As Short
+        us.UnsafeStaticCast(s)
+        Assert.AreEqual(CShort(-1), s)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastUShort()
+        Dim s As Short = -1
+        Dim us As UShort
+        s.UnsafeStaticCast(us)
+        Assert.AreEqual(CUShort(&HFFFF), us)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastInteger()
+        Dim us As UInteger = &HFFFFFFFFUI
+        Dim s As Integer
+        us.UnsafeStaticCast(s)
+        Assert.AreEqual(-1, s)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastUInteger()
+        Dim s As Integer = -1
+        Dim us As UInteger
+        s.UnsafeStaticCast(us)
+        Assert.AreEqual(&HFFFFFFFFUI, us)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastLong()
+        Dim us As ULong = &HFFFFFFFFFFFFFFFFUL
+        Dim s As Long
+        us.UnsafeStaticCast(s)
+        Assert.AreEqual(-1L, s)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastULong()
+        Dim s As Long = -1
+        Dim us As ULong
+        s.UnsafeStaticCast(us)
+        Assert.AreEqual(&HFFFFFFFFFFFFFFFFUL, us)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastSingle()
+        Dim us As Double = 1.2345
+        Dim s As Single
+        us.UnsafeStaticCast(s)
+        Assert.AreEqual(1.2345F, s, 0.0000001)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastDouble()
+        Dim us As Single = 1.2345F
+        Dim s As Double
+        us.UnsafeStaticCast(s)
+        Assert.AreEqual(1.2345, s, 0.0000001)
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastIntPtr()
+        If IntPtr.Size = 8 Then
+            Dim uptr As New UIntPtr(&HFFFFFFFFFFFFFFFFUL)
+            Dim ptr As IntPtr
+            uptr.UnsafeStaticCast(ptr)
+            Assert.AreEqual(New IntPtr(-1L), ptr)
+        ElseIf IntPtr.Size = 4 Then
+            Dim uptr As New UIntPtr(&HFFFFFFFFUI)
+            Dim ptr As IntPtr
+            uptr.UnsafeStaticCast(ptr)
+            Assert.AreEqual(New IntPtr(-1), ptr)
+        End If
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastUIntPtr()
+        If IntPtr.Size = 8 Then
+            Dim ptr As New IntPtr(-1L)
+            Dim uptr As UIntPtr
+            ptr.UnsafeStaticCast(uptr)
+            Assert.AreEqual(New UIntPtr(&HFFFFFFFFFFFFFFFFUL), uptr)
+        ElseIf IntPtr.Size = 4 Then
+            Dim ptr As New IntPtr(-1)
+            Dim uptr As UIntPtr
+            ptr.UnsafeStaticCast(uptr)
+            Assert.AreEqual(New UIntPtr(&HFFFFFFFFUI), uptr)
+        End If
+    End Sub
+
+    <TestMethod>
+    Sub TestStaticCastEnumHasFlag()
+        Dim enumValue = TestEnum.Flag3 Or TestEnum.Flag1 Or TestEnum.Flag6
+        Dim enumLongValue As Long
+        enumValue.UnsafeStaticCast(enumLongValue)
+        Assert.AreEqual(CLng(enumValue), enumLongValue)
+        Assert.IsTrue(enumLongValue And TestEnum.Flag1)
+        Assert.IsFalse(enumLongValue And TestEnum.Flag2)
+        Assert.IsTrue(enumLongValue And TestEnum.Flag3)
+        Assert.IsFalse(enumLongValue And TestEnum.Flag4)
+        Assert.IsFalse(enumLongValue And TestEnum.Flag5)
+        Assert.IsTrue(enumLongValue And TestEnum.Flag6)
+    End Sub
+
+    <Flags>
+    Enum TestEnum
+        None
+        Flag1 = 1
+        Flag2 = 2
+        Flag3 = 4
+        Flag4 = 8
+        Flag5 = 16
+        Flag6 = 32
+    End Enum
 End Class
 
 Public Class ClassWithPrivates
--- Nukepayload2.CompilerServices/UnsafeConversions.vb
+++ Nukepayload2.CompilerServices/UnsafeConversions.vb
@@ -1,5 +1,7 @@
 Imports System.ComponentModel
 Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
 Namespace Unsafe
     Public Module UnsafeConversions
         ''' <summary>
@@ -78,112 +80,148 @@ Namespace Unsafe
         End Function
 #Enable Warning BC42105
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As SByte)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.i1
-        '    ' stind.i1
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="SByte"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As SByte)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.i1
+            ' stind.i1
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As Byte)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.u1
-        '    ' stind.i1
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="Byte"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As Byte)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.u1
+            ' stind.i1
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As Short)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.i2
-        '    ' stind.i2
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="Short"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As Short)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.i2
+            ' stind.i2
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As UShort)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.u2
-        '    ' stind.i2
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="UShort"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As UShort)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.u2
+            ' stind.i2
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As Integer)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.i4
-        '    ' stind.i4
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="Integer"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As Integer)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.i4
+            ' stind.i4
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As UInteger)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.u4
-        '    ' stind.i4
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="UInteger"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As UInteger)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.u4
+            ' stind.i4
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As Long)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.i8
-        '    ' stind.i8
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="Long"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As Long)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.i8
+            ' stind.i8
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As ULong)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.u8
-        '    ' stind.i8
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="ULong"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As ULong)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.u8
+            ' stind.i8
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc)(value As TSrc, ByRef result As IntPtr)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.i
-        '    ' stind.i
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value or a reference to <see cref="IntPtr"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc)(value As TSrc, <Out> ByRef result As IntPtr)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.i
+            ' stind.i
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc)(value As TSrc, ByRef result As UIntPtr)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.u
-        '    ' stind.i
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value or a reference to <see cref="UIntPtr"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc)(value As TSrc, <Out> ByRef result As UIntPtr)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.u
+            ' stind.i
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As Single)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.r4
-        '    ' stind.r4
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="Single"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As Single)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.r4
+            ' stind.r4
+        End Sub
 
-        '<Extension>
-        '<MethodImpl(MethodImplOptions.AggressiveInlining)>
-        'Public Sub StaticCast(Of TSrc As Structure)(value As TSrc, ByRef result As Double)
-        '    ' ldarg.1
-        '    ' ldarg.0
-        '    ' conv.r8
-        '    ' stind.r8
-        'End Sub
+        ''' <summary>
+        ''' Converts an unmanaged value to <see cref="Double"/>.
+        ''' </summary>
+        <Extension>
+        <MethodImpl(MethodImplOptions.ForwardRef Or MethodImplOptions.AggressiveInlining)>
+        Public Sub UnsafeStaticCast(Of TSrc As Structure)(value As TSrc, <Out> ByRef result As Double)
+            ' ldarg.1
+            ' ldarg.0
+            ' conv.r8
+            ' stind.r8
+        End Sub
     End Module
 End Namespace
